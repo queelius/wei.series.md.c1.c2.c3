2532f9bb7d54424326af656c5afde5829f8efd1e
queelius <lex@metafunctor.com>
2023-09-30 17:37:36 -0500

    updates

--- data-raw/alex_weibull_series_ex.R
+++ data-raw/alex_weibull_series_ex.R
@@ -3,18 +3,20 @@
 # example in Guo, table 2, but with two new components
 # added. Each component has approximately the same
 # reliability, so that there is no clear weakest link.
-#
-#   theta <- c(
-#       k1 = 1.2576, lambda1 = 994.3661,
-#       k2 = 1.1635, lambda2 = 908.9458,
-#       k3 = 1.1308, lambda3 = 840.1141,
-#       k4 = 1.1802, lambda4 = 940.1342,
-#       k5 = 1.2034, lambda5 = 923.1631)
-
 library(usethis)
+
+theta <- c(shape1 = 1.2576, scale1 = 994.3661,
+           shape2 = 1.1635, scale2 = 908.9458,
+           shape3 = 1.1308, scale3 = 840.1141,
+           shape4 = 1.1802, scale4 = 940.1342,
+           shape5 = 1.2034, scale5 = 923.1631)
+shapes <- theta[seq(1, length(theta), 2)]
+scales <- theta[seq(2, length(theta), 2)]
+
 alex_weibull_series <- list(
-    shapes = c(1.2576, 1.1635, 1.1308, 1.1802, 1.2034),
-    scales = c(994.3661, 908.9458, 840.1141, 940.1342, 923.1631),
+    theta = theta,
+    shapes = shapes,
+    scales = scales,
     family = "weibull series system")
 
 usethis::use_data(alex_weibull_series, overwrite = TRUE)
